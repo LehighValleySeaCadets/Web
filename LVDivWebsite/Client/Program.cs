1eb9de15fce15fc56146ce7b3a3099ac74e96674
David J. Sweeney <david.j.sweeney@outlook.com>
2023-08-18 22:22:54 -0400

    Added basic footer.  Needs more formatting and content.

--- LVDivWebsite/Client/Program.cs
+++ LVDivWebsite/Client/Program.cs
@@ -13,6 +13,7 @@ namespace LVDivWebsite
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
 
             await builder.Build().RunAsync();
         }
